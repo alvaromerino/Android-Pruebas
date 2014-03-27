95c828554f705246de935ebb5b758df024b6cb2f
alvaromerino <alvaromerinogarcia@gmail.com>
2014-03-27 11:36:22 +0100

    HolaMundo

---
+++ android-holamundo/src/com/alvaro93mg/android/holamundo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alvaro93mg.android.holamundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alvaro on 24/03/14.
+ */
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
